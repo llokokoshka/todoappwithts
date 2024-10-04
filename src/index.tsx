c29dfde24bceab31fe90dac5147d1314aad5b1ef
Katerina <katelokota@gmail.com>
2024-10-04 11:22:36 +0300

    Add routes

--- src/index.tsx
+++ src/index.tsx
@@ -6,17 +6,29 @@ import { Provider } from 'react-redux';
 import GlobalStyles from './styles/global'
 import { ThemeProvider } from 'styled-components';
 import { baseTheme } from './styles/theme';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 
 const rootElem = document.getElementById('root');
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
 
 if (rootElem) {
   const root = ReactDOM.createRoot(rootElem);
   root.render(
     <React.StrictMode>
+
       <ThemeProvider theme={baseTheme}>
         <Provider store={store}>
           <GlobalStyles />
-          <App />
+          <RouterProvider router={router} />
         </Provider>
       </ThemeProvider>
     </React.StrictMode>
